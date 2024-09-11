157aa387766500f6840f654533c424edd84293c8
sahil-raj <sahilrajaaru@gmail.com>
2024-09-12 01:43:25 +0530

    buyTicket complete

--- contracts/ETickH.sol
+++ contracts/ETickH.sol
@@ -1,10 +1,11 @@
 //SPDX-License-Identifier: MIT
 //contracts/ETickH.sol
-
 pragma solidity ^0.8.4;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+///@title
+///@author github.com/sahil-raj
 contract ETickH is Ownable {
     constructor(address _initOwner) Ownable(_initOwner) {}
 
@@ -21,7 +22,8 @@ contract ETickH is Ownable {
         address[] organizers;
     }
 
-    mapping(address => ETickHEvent[]) internal eventMapping;
+    mapping(address => ETickHEvent[]) public eventMapping;
+    mapping(uint => ETickHEvent) internal idToEventMap;
 
     modifier createETickH(string memory _eventName, uint _startTime, uint _eventDuration, uint _tktCount) {
         require(bytes(_eventName).length > 0, "Event must have a name");
@@ -30,36 +32,45 @@ contract ETickH is Ownable {
         _;
     }
 
+    modifier buyTicketMod(address _eventCreator, uint _eventId, uint _ticketCount) {
+        //check if the creator by user and creator by eventId matches
+        require(idToEventMap[_eventId].creator == _eventCreator, "creator missmatch");
+        //check if tickets are available
+        require(idToEventMap[_eventId].ticket_count >= _ticketCount);
+        //msg.value must be equal or greater than req.
+        require(msg.value >= idToEventMap[_eventId].ticket_price*_ticketCount, "insuffcient funds");
+        _;
+    }
+
     function createEvent(string memory _eventName, uint _startTime, uint _eventDuration, uint _tktCount, uint _tktPrice, address[] memory _orgs) createETickH(_eventName, _startTime, _eventDuration, _tktCount) public returns(bool) {
 
         ETickHEvent memory myEvent = ETickHEvent(++eventCount, _eventName, _startTime, _eventDuration, _tktCount, _tktPrice, payable(msg.sender), _orgs);
 
         eventMapping[msg.sender].push(myEvent);
+        idToEventMap[eventCount] = myEvent;
 
         return true;
     }
 
-    function buyTicket(address _eventCreator, uint _eventId, uint _ticketCount) public payable returns(bool) {
+    function buyTicket(address _eventCreator, uint _eventId, uint _ticketCount) buyTicketMod(_eventCreator, _eventId, _ticketCount) public payable returns(bool) {
+
         bool success = false;
 
-        for (uint i=0; i< eventMapping[_eventCreator].length; ++i) {
-            if (eventMapping[_eventCreator][i].event_id == _eventId) {
-                success = eventMapping[_eventCreator][i].creator.send(msg.value);
-                break;
-            }
-        }
+        // for (uint i=0; i< eventMapping[_eventCreator].length;) {
+        //     if (eventMapping[_eventCreator][i].event_id == _eventId) {
+        //         success = eventMapping[_eventCreator][i].creator.send(msg.value);
+        //         break;
+        //     }
+        //     unchecked {
+        //         ++i;
+        //     }
+        // }
+
+        success = idToEventMap[_eventId].creator.send(msg.value);
+
+        idToEventMap[_eventId].ticket_count -= _ticketCount;
 
         return success;
     }
 
 }
-
-// address payable recipient = 0x1234567890abcdef1234567890abcdef12345678;
-
-// function sendEtherUsingSend() public payable {
-//     bool success = recipient.send(msg.value);
-//     if (!success) {
-//         // Handle failure (e.g., revert the transaction or take other action)
-//         revert("Ether transfer failed.");
-//     }
-// }
